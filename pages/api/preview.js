7b96e199ff838a1eff7e30881f72817592cd7410
Micurran <mcurran1722@gmail.com>
2022-03-15 16:46:27 -0600

    add chromium for serverless

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,10 +1,8 @@
-import puppeteer from "puppeteer";
 const chromium = require('chrome-aws-lambda');
 
 export default async function handler(req, res) {
   try {
     let { url } = req.query;
-
     let image = await getImageBase64(url);
 
     res.status(200).json({
