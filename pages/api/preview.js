d545d1a4bc2f5346ed8e4075f783740d1dd525a8
Micurran <mcurran1722@gmail.com>
2022-03-15 19:05:52 -0600

    test

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer-core";
+import chromium from 'chrome-aws-lambda';
 
 export default async function handler(req, res) {
   try {
@@ -10,17 +10,43 @@ export default async function handler(req, res) {
       image,
     });
   } catch (error) {
-    res.status(500).json({
+    console.log(JSON.stringify(error))
+    res.status(200).json({
       error: JSON.stringify(error),
     });
   }
 }
 
 let getImageBase64 = async (url) => {
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
-  await page.goto(url);
-  let image = await page.screenshot({ encoding: "base64" });
-  await browser.close();
+  let image = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto(url || 'https://example.com');
+    image = await page.screenshot({ encoding: "base64" });
+    await browser.close();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
   return image;
+  //let browser = await puppeteer.launch();
+/*   let page = await browser.newPage();
+  await page.goto(url);
+  await browser.close();
+  return image; */
 };
